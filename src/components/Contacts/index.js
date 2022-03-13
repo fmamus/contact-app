81beaf8c6f35a8a0c44807cb130b7a2a96ed298c
mamus <furkanmamus1903@hotmail.com>
2022-03-13 17:42:55 +0300

    files upload

---
+++ src/components/Contacts/index.js
@@ -0,0 +1,36 @@
+import {useState, useEffect} from 'react';
+
+import './style.css'
+import List from './List';
+import Form from './Form';
+
+function Contacts() {
+  const [contacts, setContacts] = useState([
+    {
+      fullname: "Furkan",
+      phone_number: 1234,
+    },
+    {
+      fullname: "Fatma",
+      phone_number: 5678,
+    },
+    {
+      fullname: "Ali",
+      phone_number: 9514,
+    },
+  ]);
+
+  useEffect(() => {
+    console.log(contacts);
+  },[contacts])
+
+  return (
+      <div id='container'>
+        <h1>Contacts</h1>
+          <List contacts = {contacts}/>
+          <Form addContact = {setContacts} contacts = {contacts}/>
+      </div>
+  )
+}
+
+export default Contacts;
